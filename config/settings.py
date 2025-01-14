aa5c8ade886968fae2c003b675e999e3db2a2382
kneeraazon404 <kneeraazon@gmail.com>
2025-01-14 14:34:58 +0545

    first phase ready

--- config/settings.py
+++ config/settings.py
@@ -1,7 +1,7 @@
 # config/settings.py
 HEDGE_SETTINGS = {
     "min_hedge_size": 0.01,
-    "max_hedge_size": 90.0,
+    "max_hedge_size": 100.0,
     "hedge_interval": 60,  # seconds
     "default_rate": 0.05,  # Risk-free rate
     "min_volatility": 0.001,
